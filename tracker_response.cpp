8c64341648b1fddaf08bd7b73788b7fbc33285d6
BenJopflik <ben.jopflik@gmail.com>
2015-06-25 16:54:03 +0300

    mv tracker_response on token_list_processor

--- tracker_response.cpp
+++ tracker_response.cpp
@@ -4,124 +4,151 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include "common/ip_addr.hpp"
 
-TrackerResponse::TrackerResponse(const std::vector<BeToken> & tokens, const char * source_, uint64_t size)
+TrackerResponse::TrackerResponse(const std::vector<BeToken> & tokens, const char * source, uint64_t size)
 {
-    std::string source(source_, size); // TODO remove
-    uint64_t current_token = 0;
-    auto filler = m_fillers.end();
-    uint64_t type = tokens[current_token++].type();
+    m_fillers = {
+                    {"peers",           &TrackerResponse::fill_peers},
+                    {"interval",        &TrackerResponse::fill_interval},
+                    {"complete",        &TrackerResponse::fill_complete},
+                    {"incomplete",      &TrackerResponse::fill_incomplete},
+                    {"tracker id",      &TrackerResponse::fill_tracker_id},
+                    {"min interval",    &TrackerResponse::fill_min_interval},
+                    {"failure reason",  &TrackerResponse::fill_failure_reason},
+                    {"warning message", &TrackerResponse::fill_warning}
+                };
+
+    uint64_t type = tokens[0].type();
 
     if (type != BeToken::DICT_START)
         throw std::runtime_error("invalid tracker respons. not a dict");
 
-    const uint64_t TOKENS_SIZE = tokens.size();
-    while (current_token < TOKENS_SIZE)
-    {
-        type = tokens[current_token].type();
-        if (type == BeToken::INT || type == BeToken::STR)
-        {
-            filler = m_fillers.find(tokens[current_token].str(source));
-            if (filler != m_fillers.end())
-            {
-                (this->*(filler->second))(tokens, source, current_token);
-                continue;
-            }
-        }
+    process(tokens, {source, size});
 
-        ++current_token;
-    }
+    if (m_failure_reason.empty() && m_warning.empty() && !m_peers.empty())
+        m_valid = true;
 }
 
-void TrackerResponse::fill_int(uint64_t & dest, const std::vector<BeToken> & tokens, const std::string & source, uint64_t & current_token)
-{
-    ++current_token;
-    uint64_t type = tokens[current_token].type();
-    if (type == BeToken::INT)
-        dest = std::stoll(tokens[current_token].str(source));
-
-    ++current_token;
-}
-
-void TrackerResponse::fill_str(std::string & dest, const std::vector<BeToken> & tokens, const std::string & source, uint64_t & current_token)
-{
-    ++current_token;
-    uint64_t type = tokens[current_token].type();
-    if (type == BeToken::STR)
-        dest = tokens[current_token].str(source);
-
-    ++current_token;
-}
-
-void TrackerResponse::fill_failure_reason(const std::vector<BeToken> & tokens, const std::string & source, uint64_t & current_token)
+MEMBER_DEFINE(TrackerResponse, fill_failure_reason)
 {
     fill_str(m_failure_reason, tokens, source, current_token);
 }
 
-void TrackerResponse::fill_warning(const std::vector<BeToken> & tokens, const std::string & source, uint64_t & current_token)
+MEMBER_DEFINE(TrackerResponse, fill_warning)
 {
     fill_str(m_warning, tokens, source, current_token);
 }
 
-void TrackerResponse::fill_interval(const std::vector<BeToken> & tokens, const std::string & source, uint64_t & current_token)
+MEMBER_DEFINE(TrackerResponse, fill_interval)
 {
     fill_int(m_interval, tokens, source, current_token);
 }
 
-void TrackerResponse::fill_min_interval(const std::vector<BeToken> & tokens, const std::string & source, uint64_t & current_token)
+MEMBER_DEFINE(TrackerResponse, fill_min_interval)
 {
     fill_int(m_min_interval, tokens, source, current_token);
 }
 
-void TrackerResponse::fill_tracker_id(const std::vector<BeToken> & tokens, const std::string & source, uint64_t & current_token)
+MEMBER_DEFINE(TrackerResponse, fill_tracker_id)
 {
     fill_str(m_tracker_id, tokens, source, current_token);
 }
 
-void TrackerResponse::fill_complete(const std::vector<BeToken> & tokens, const std::string & source, uint64_t & current_token)
+MEMBER_DEFINE(TrackerResponse, fill_complete)
 {
     fill_int(m_complete, tokens, source, current_token);
 }
 
-void TrackerResponse::fill_incomplete(const std::vector<BeToken> & tokens, const std::string & source, uint64_t & current_token)
+MEMBER_DEFINE(TrackerResponse, fill_incomplete)
 {
     fill_int(m_incomplete, tokens, source, current_token);
 }
 
-void TrackerResponse::fill_peers(const std::vector<BeToken> & tokens, const std::string & source, uint64_t & current_token)
+MEMBER_DEFINE(TrackerResponse, fill_peers)
 {
     ++current_token;
     uint64_t type = tokens[current_token].type();
     if (type == BeToken::STR)
         return fill_peers_compact(tokens, source, current_token);
-
-    current_token = tokens.size();
+    else
+        return fill_peers_detail(tokens, source, current_token);
 }
 
-void TrackerResponse::fill_peers_compact(const std::vector<BeToken> & tokens, const std::string & source, uint64_t & current_token)
+MEMBER_DEFINE(TrackerResponse, fill_peers_compact)
 {
-    std::string peers = tokens[current_token].str(source);
-
-    uint64_t number_of_peers = peers.size() / 6; // 4 - ip 2 - port in network notation
+    uint64_t number_of_peers = tokens[current_token].substr().size() / 6; // 4 - ip 2 - port in network notation
     m_peers.resize(number_of_peers);
 
     char buf[INET_ADDRSTRLEN];
-    const char * curr = peers.c_str();
-    for (int i = 0; i < number_of_peers; ++i)
+    const char * curr = source.data() + tokens[current_token].substr().start();
+    sockaddr_in * si = nullptr;
+
+    for (uint64_t i = 0; i < number_of_peers; ++i)
     {
         if (inet_ntop(AF_INET, curr, buf, INET_ADDRSTRLEN))
             m_peers[i].ip = buf;
         else
             throw std::runtime_error("invalid ip");
 
+        si = &m_peers[i].saddr;
+        si->sin_family = AF_INET;
+        si->sin_addr.s_addr = *(uint32_t *)curr;
+
         curr += 4;
+
         m_peers[i].port = ntohs(*(uint16_t *)curr);
+        si->sin_port = *(uint16_t *)curr;
+
         curr += 2;
     }
 
     ++current_token;
 }
 
+MEMBER_DEFINE(TrackerResponse, fill_peers_detail)
+{
+    uint64_t type = tokens[current_token].type();
+    if (type != BeToken::LIST_START)
+        throw std::runtime_error("invalid betype. expected list_start");
+
+    ++current_token;
+
+    std::string key = "";
+    std::string value = "";
+
+    const uint64_t TOKEN_SIZE = tokens.size();
+    while (current_token < TOKEN_SIZE)
+    {
+        type = tokens[current_token].type();
+        switch (type)
+        {
+            case BeToken::DICT_START:
+                m_peers.push_back(Peer());
+                break;
+
+            case BeToken::DICT_END:
+                m_peers.back().saddr = IpAddr::get_sockaddr_in(m_peers.back().ip, m_peers.back().port);
+                break;
+
+            case BeToken::STR:
+                key = tokens[current_token].str(source);
+                ++current_token;
+                value = tokens[current_token].str(source);
+
+                if (key == "peer id")
+                        m_peers.back().id = value;
+                else if (key == "ip")
+                        m_peers.back().ip = value;
+                else if (key == "port")
+                        m_peers.back().port = stol(value);
+
+                break;
+        }
+
+        ++current_token;
+    }
+}
 
 void TrackerResponse::print() const
 {
@@ -141,5 +168,5 @@ void TrackerResponse::print() const
         std::cerr << "\tport: " << i.port << std::endl;
         std::cerr << std::endl;
     }
-
 }
+#undef MEMBER_DEFINE
--- tracker_response.hpp
+++ tracker_response.hpp
@@ -2,63 +2,64 @@
 #include "bedecoder/be_token.hpp"
 #include <vector>
 #include <unordered_map>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include "token_list_processor.hpp"
 
 struct Peer
 {
-    std::string id {""};
-    std::string ip {""};
+    std::string id   {""};
+    std::string ip   {""};
     uint16_t    port {0};
 
-    sockaddr    saddr;
+    sockaddr_in    saddr;
 };
 
-class TrackerResponse
+class TrackerResponse : public TokenListProcessor<TrackerResponse>
 {
 public:
     TrackerResponse(const std::vector<BeToken> & tokens, const char * source, uint64_t size);
 
+    operator bool () const {return m_valid;}
+
     void print() const;
 
-private:
-    void fill_failure_reason(const std::vector<BeToken> & tokens, const std::string & source, uint64_t & current_token);
-    void fill_warning(const std::vector<BeToken> & tokens, const std::string & source, uint64_t & current_token);
-    void fill_interval(const std::vector<BeToken> & tokens, const std::string & source, uint64_t & current_token);
-    void fill_min_interval(const std::vector<BeToken> & tokens, const std::string & source, uint64_t & current_token);
-    void fill_tracker_id(const std::vector<BeToken> & tokens, const std::string & source, uint64_t & current_token);
-    void fill_complete(const std::vector<BeToken> & tokens, const std::string & source, uint64_t & current_token);
-    void fill_incomplete(const std::vector<BeToken> & tokens, const std::string & source, uint64_t & current_token);
-    void fill_peers(const std::vector<BeToken> & tokens, const std::string & source, uint64_t & current_token);
-    void fill_peers_compact(const std::vector<BeToken> & tokens, const std::string & source, uint64_t & current_token);
+    uint64_t interval() const {return m_interval;}
+    uint64_t min_interval() const {return m_min_interval;}
+    uint64_t complete() const {return m_complete;}
+    uint64_t incomplete() const {return m_incomplete;}
 
-    void fill_str(std::string & dest, const std::vector<BeToken> & tokens, const std::string & source, uint64_t & current_token);
-    void fill_int(uint64_t & dest, const std::vector<BeToken> & tokens, const std::string & source, uint64_t & current_token);
+    const std::vector<Peer> & peers() const {return m_peers;}
 
-
+    const std::string & failure_reason() const {return m_failure_reason;}
+    const std::string & warning() const {return m_warning;}
+    const std::string & traker_id() const {return m_tracker_id;}
 
 private:
-    const std::unordered_map<std::string, decltype(&TrackerResponse::fill_failure_reason)> m_fillers {
-                                                                                        {"failure reason", &TrackerResponse::fill_failure_reason},
-                                                                                        {"warning message", &TrackerResponse::fill_warning},
-                                                                                        {"interval", &TrackerResponse::fill_interval},
-                                                                                        {"min interval", &TrackerResponse::fill_min_interval},
-                                                                                        {"tracker id", &TrackerResponse::fill_tracker_id},
-                                                                                        {"complete", &TrackerResponse::fill_complete},
-                                                                                        {"incomplete", &TrackerResponse::fill_incomplete},
-                                                                                        {"peers", &TrackerResponse::fill_peers}
+    MEMBER_DECLARE(fill_failure_reason)
+    MEMBER_DECLARE(fill_warning)
+    MEMBER_DECLARE(fill_interval)
+    MEMBER_DECLARE(fill_min_interval)
+    MEMBER_DECLARE(fill_tracker_id)
+    MEMBER_DECLARE(fill_complete)
+    MEMBER_DECLARE(fill_incomplete)
+    MEMBER_DECLARE(fill_peers)
+    MEMBER_DECLARE(fill_peers_compact)
+    MEMBER_DECLARE(fill_peers_detail)
 
-                                                                                      };
-
-
+private:
     std::vector<Peer> m_peers;
 
     std::string m_failure_reason {""};
-    std::string m_warning {""};
-    std::string m_tracker_id {""};
+    std::string m_warning        {""};
+    std::string m_tracker_id     {""};
 
-    uint64_t m_interval {0}; // in seconds
+    uint64_t m_interval     {0}; // in seconds
     uint64_t m_min_interval {0}; // in seconds
-    uint64_t m_complete {0};
-    uint64_t m_incomplete {0};
+    uint64_t m_complete     {0};
+    uint64_t m_incomplete   {0};
 
-
+    bool m_valid {false};
 };
+#undef MEMBER_DECLARE
+
