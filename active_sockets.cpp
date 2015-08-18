37595ea3f9af689c943b20dd5ce32f359d203cc5
BenJopflik <ben.jopflik@gmail.com>
2015-08-18 09:41:31 +0300

    active_sockets

--- active_sockets.cpp
+++ active_sockets.cpp
@@ -12,17 +12,21 @@ ActiveSockets::~ActiveSockets()
 
 }
 
-bool ActiveSockets::add(std::unique_ptr<Socket> socket)
+bool ActiveSockets::add(const LightPeerInfo & lpi)
 {
     std::lock_guard<decltype(m_lock)> lock(m_lock);
 
-    uint64_t fd = socket->get_fd();
-
-    return m_sockets.emplace(fd, std::move(socket)).second;
+    return m_peers.insert(lpi).second;
 }
 
-bool ActiveSockets::remove(uint64_t fd)
+bool ActiveSockets::remove(const LightPeerInfo & lpi)
 {
     std::lock_guard<decltype(m_lock)> lock(m_lock);
-    return m_sockets.erase(fd);
+    return m_peers.erase(lpi);
+}
+
+uint64_t ActiveSockets::size() const
+{
+    std::lock_guard<decltype(m_lock)> lock(m_lock);
+    return m_peers.size();
 }
--- active_sockets.hpp
+++ active_sockets.hpp
@@ -1,9 +1,27 @@
 #pragma once
-#include <unordered_map>
+#include <unordered_set>
 #include <memory>
 
+#include "common/hashable.hpp"
+#include "common/hasher.hpp"
 #include "socket/socket.hpp"
 #include "thread/spinlock.hpp"
+
+struct LightPeerInfo : public Hashable<LightPeerInfo>
+{
+public:
+    LightPeerInfo(uint32_t address, uint16_t port) : address(address), port(port) {}
+    uint64_t hash() const {return std::hash<uint64_t>()(*(uint64_t *)(&this->address));}
+    bool operator == (const LightPeerInfo & right) const {return address == right.address && port == right.port;}
+
+public:
+    uint32_t address {0};
+    uint16_t port    {0};
+
+private:
+    uint16_t padding {0};
+};
+
 
 class ActiveSockets
 {
@@ -11,11 +29,12 @@ public:
     ActiveSockets();
    ~ActiveSockets();
 
-    bool add(std::unique_ptr<Socket>);
-    bool remove(uint64_t fd);
+    bool add(const LightPeerInfo & peer);
+    bool remove(const LightPeerInfo & peer);
+    uint64_t size() const;
 
 private:
-    std::unordered_map<uint64_t, std::unique_ptr<Socket>> m_sockets;
-    SpinlockYield m_lock;
+    std::unordered_set<LightPeerInfo, Hasher<LightPeerInfo>> m_peers;
+    mutable SpinlockYield m_lock;
 
 };
